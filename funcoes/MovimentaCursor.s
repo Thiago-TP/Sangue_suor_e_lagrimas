121ade0baba3d2c4bc446c4cdfff8ef2243537eb
root <root@LAPTOP-D31HN8I8.localdomain>
2022-09-28 19:25:39 -0300

    integrando desvios e ataque

--- funcoes/MovimentaCursor.s
+++ funcoes/MovimentaCursor.s
@@ -36,6 +36,47 @@ FimMovimentaCursor:
 	ret					# retorno para a funcao chamadora
 CHAR_ENTER:
 	la	a0, PosicaoAtualCursor
+	la	t0, AtaqueAtivado
+	lb	t1, 0(t0)
+	beqz	t1,PulaDecideAtaque
+	lw	t4,0(a0)
+	lw	t5,4(a0)
+	li	t6, 1					
+LoopVerificaInimigo.MoviCursor:
+	mv	a7, t6
+	call EscolhePersonagem # retorna em a1
+	lw	t0, 0(a1)
+	lw	t1, 4(a1)
+	bne	t0,t4,PulaVerificaInimigo.MoviCursor
+	bne	t1,t5,PulaVerificaInimigo.MoviCursor
+	addi	t4, t4,2
+	addi	t5, t5,2
+	li 	t0, 0xFF200604 		# carrega em t0 o endereco de troca de frame
+	lw 	t1, 0(t0) 		# troca de frame
+	xori	t1, t1, 1
+	li 	t0, 0xFF0 # carrega 0xFF0 em t0
+	add 	t0, t0,t1 # adiciona o frame ao FF0 (se o frame for 1 vira FF1, se for 0 fica FF0)
+	slli 	t0, t0,20 # shift de 20 bits pra esquerda (0xFF0 vira 0xFF000000, 0xFF1 vira 0xFF100000)
+	add 	t0, t0,t4 # adiciona x ao t0
+	li 	t1, 320 # t1 = 320
+	mul 	t1, t1,t5 # multiplica y por t1
+	add 	t0, t0,t1 # coloca o valor final do calculo do endere√ßo em t0
+	lb	t1, 0(t0)
+	li	t3, 20
+	bne	t1, t3,FimMovimentaCursor
+	la	t0, AtaqueAtivado
+	sb	zero, 0(t0)
+	
+	#################################################
+	#	 aqui È onde chamariamos a luta		#
+	#################################################
+	
+	j	FimMovimentaCursor
+PulaVerificaInimigo.MoviCursor:
+	addi	t6, t6, 2
+	ble	t6, s4, LoopVerificaInimigo.MoviCursor
+	j	FimMovimentaCursor
+PulaDecideAtaque:
 	call	DecideMudanca
 	j	FimMovimentaCursor
 CHAR_ESQ:
